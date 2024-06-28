8f23296f84ab761f79d70351754822fc7d11fa8b
kondo kazuki <zaku2590@gmail.com>
2024-06-28 11:54:26 +0900

    User name added

--- coin.py
+++ coin.py
@@ -17,10 +17,6 @@ def main():
     tails_count = results.count("Tails")
     
     print(f"Heads: {heads_count}, Tails: {tails_count}")
-    if heads_count > tails_count:
-        print(f"{user_name} won!")
-    else:
-        print(f"{user_name} lost!")
 
 if __name__ == "__main__":
     main()
