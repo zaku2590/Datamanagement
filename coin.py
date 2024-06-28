4bae39a0dc6d03caa2a7ac7647c1310a71fa02e3
kondo kazuki <zaku2590@gmail.com>
2024-06-28 11:45:22 +0900

    HoT: initial revision

--- coin.py
+++ coin.py
@@ -14,11 +14,6 @@ def main():
     tails_count = results.count("Tails")
     
     print(f"Heads: {heads_count}, Tails: {tails_count}")
-    
-    if heads_count > tails_count:
-        print("You won!")
-    else:
-        print("You lost!")
 
 if __name__ == "__main__":
     main()
